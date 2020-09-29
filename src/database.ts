5ef97eef86969831a2bc55ca6a78880d5a4c3fec
Don2Quixote <idonquixote@yandex.ru>
2020-09-29 10:50:42 +0300

    Added game logic

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,67 @@
 import { IUser, IUserRow } from './user.d'
 import * as mysql from 'mysql2'
 
+export async function updateBalance(bd: mysql.Connection, value: number): Promise<boolean> {
+    if (!Number.isInteger(value)) Promise.reject('Not integer passed')
+    return new Promise((resolve, reject) => {
+        bd.query(`UPDATE balance SET balance = balance + ${value}`, (err, queryResult) => {
+            if (err) reject(err)
+            else resolve(true)
+        })
+    })
+}
+
+export interface IStake {
+    userID: number,
+    prediction: string
+}
+
+export async function getFreeStakes(bd: mysql.Connection): Promise<IStake[]> {
+    return new Promise((resolve, reject) => {
+        bd.query('SELECT * FROM freeStakes', (err, queryResult) => {
+            if (err) reject(err)
+            else resolve(queryResult as IStake[])
+        })
+    })
+}
+
+export async function getStakes(bd: mysql.Connection): Promise<IStake[]> {
+    return new Promise((resolve, reject) => {
+        bd.query('SELECT * FROM stakes', (err, queryResult) => {
+            if (err) reject(err)
+            else resolve(queryResult as IStake[])
+        })
+    })
+}
+
+export async function truncateFreeStakes(bd: mysql.Connection): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        bd.query('TRUNCATE freeStakes', (err, queryResult) => {
+            if (err) reject(err)
+            else {
+                bd.query('UPDATE users SET freeStake = ""', (err, queryResult) => {
+                    if (err) reject(err)
+                    else resolve(true)
+                })
+            }
+        })
+    })
+}
+
+export async function truncateStakes(bd: mysql.Connection): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        bd.query('TRUNCATE stakes', (err, queryResult) => {
+            if (err) reject(err)
+            else {
+                bd.query('UPDATE users SET stakes = ""', (err, queryResult) => {
+                    if (err) reject(err)
+                    else resolve(true)
+                })
+            }
+        })
+    })
+}
+
 export async function addFreeStake(bd: mysql.Connection, userID: number, prediction: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
         bd.query(`INSERT INTO freeStakes (userID, prediction) VALUES (${userID}, "${prediction}")`, (err, queryResult) => {
@@ -42,6 +103,7 @@ export async function getUser(bd: mysql.Connection, id: number): Promise<IUser> 
                             id: user.id,
                             name: user.name,
                             vip: user.vip,
+                            ban: user.ban,
                             referrer: user.referrer,
                             referrals: user.referrals,
                             awaitingMessage: user.awaitingMessage,
@@ -68,6 +130,7 @@ export async function addUser(bd: mysql.Connection, id: number, name: string, re
                 id,
                 name,
                 vip,
+                ban,
                 referrer,
                 referrals,
                 awaitingMessage,
@@ -79,7 +142,7 @@ export async function addUser(bd: mysql.Connection, id: number, name: string, re
                 freeStake,
                 stakes
             )
-            VALUES (${id}, "${name}", 0, ${referrerID ? referrerID : 0}, 0, "", "", "", 0, 0, 0, "", "")
+            VALUES (${id}, "${name}", 0, 0, ${referrerID ? referrerID : 0}, 0, "", "", "", 0, 0, 0, "", "")
         `,
         (err, res) => {
             if (err) reject(err)
@@ -88,7 +151,7 @@ export async function addUser(bd: mysql.Connection, id: number, name: string, re
     })
 }
 
-type UserRowField = 'id' | 'name' | 'vip' | 'refferer' | 'referrals' | 'awaitingMessage' | 'actionData' | 'lang' | 'balance' | 'withdrawRequest' | 'wins' | 'freeStake' | 'stakes'
+type UserRowField = 'id' | 'name' | 'vip' | 'ban' | 'refferer' | 'referrals' | 'awaitingMessage' | 'actionData' | 'lang' | 'balance' | 'withdrawRequest' | 'wins' | 'freeStake' | 'stakes'
 export async function updateUser(bd: mysql.Connection, id: number, fields: Array<UserRowField>, values: Array<string | number>): Promise<boolean>
 export async function updateUser(bd: mysql.Connection, id: number, filed: UserRowField, value: string | number): Promise<boolean>
 export async function updateUser(bd, id, fields, values) {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
+import * as fetch from 'node-fetch'
+import * as schedule from 'node-schedule'
 import * as mysql from 'mysql2'
 import { Telegraf } from 'telegraf'
 import { TelegrafContext } from 'telegraf/typings/context'
+
+import handleUserMessage from './handleUserMessage'
+import handleAdminMessage from './handleAdminMessage'
+import handleUserCallback from './handleUserCallback'
+import handleAdminCallback from './handleAdminCallback'
+
+import {
+    getFreeStakes,
+    getStakes,
+    truncateFreeStakes,
+    truncateStakes,
+    updateBalance,
+    getUser,
+    updateUser,
+    IStake
+} from './database'
 
 const sql: mysql.Connection = mysql.createConnection({
     host: process.env.SQL_HOST,
@@ -13,8 +31,6 @@ const sql: mysql.Connection = mysql.createConnection({
 
 const bot = new Telegraf(process.env.TOKEN)
 
-import handleUserMessage from './handleUserMessage'
-import handleAdminMessage from './handleAdminMessage'
 bot.on('message', async (ctx: TelegrafContext) => {
     try {
         if (ctx.from.id == +process.env.ADMIN_ID) {
@@ -28,8 +44,6 @@ bot.on('message', async (ctx: TelegrafContext) => {
     }
 })
 
-import handleUserCallback from './handleUserCallback'
-import handleAdminCallback from './handleAdminCallback'
 bot.on('callback_query', async (ctx: TelegrafContext) => {
     try {
         if (ctx.from.id == +process.env.ADMIN_ID) {
@@ -45,3 +59,257 @@ bot.on('callback_query', async (ctx: TelegrafContext) => {
 
 bot.launch()
 
+process.env.gettingStakes = 'true'
+
+function stopGettingStakes() {
+    console.log('stopGettingStakes()')
+    // process.env.* - is a string type, so can't use true/false
+    process.env.gettingStakes = ''
+}
+
+function startGettingStakes() {
+    console.log('startGettingStakes()')
+    process.env.gettingsStakes = 'true'
+}
+
+async function getCourse() {
+    console.log('getCourse()')
+    let res = await fetch('https://yobit.net/api/3/ticker/btc_usd')
+    let json = await res.json()
+    if (!json.btc_usd) throw new Error('Incorrect response from yobit')
+    return json.btc_usd.avg
+}
+
+async function playFreeGame() {
+    console.log('playFreeGame()')
+    let stakes = await getFreeStakes(sql)
+    let players = []
+    let course: number
+    try {
+        course = await getCourse()
+        await bot.telegram.sendMessage(process.env.GROUP_ID, '💲 Курс: ' + parseFloat((course * 100).toFixed()) / 100)
+    } catch (e) {
+        let groupMessage =
+            '❌ Во время бесплатной игры произошла ошибка'
+        bot.telegram.sendMessage(process.env.GROUP_ID, groupMessage)
+        let clearedPlayers = []
+        await truncateFreeStakes(sql)
+        return
+    }
+    let firstSortingIteration = true
+    stakes.sort((next, prev) => {
+        if (firstSortingIteration) {
+            firstSortingIteration = false
+            if (!players.includes(prev.userID)) players.push(prev.userID)
+            if (!players.includes(next.userID)) players.push(next.userID)
+        } else {
+            if (!players.includes(next.userID)) players.push(next.userID)
+        }
+        let prevDifferenceWithCourse = Math.abs(parseFloat(prev.prediction) - course)
+        let nextDifferenceWithCourse = Math.abs(parseFloat(next.prediction) - course)
+        return nextDifferenceWithCourse - prevDifferenceWithCourse
+    })
+
+    function isAllPredictionsEquals(stakes: IStake[]) {
+        let firstPrediction = stakes[0].prediction
+        for (let stake of stakes) {
+            if (stake.prediction != firstPrediction) return false
+        }
+        return true
+    }
+
+    if (players.length < 2) {
+        let groupMessage =
+            'ℹ️ Игра с бесплатными ставками была отменена, так как не набралось нужное количество игроков'
+        bot.telegram.sendMessage(process.env.GROUP_ID, groupMessage)
+        await truncateFreeStakes(sql)
+    } else if (isAllPredictionsEquals(stakes)) {
+        let groupMessage =
+            'ℹ️ Игра с бесплатными ставками была отменена, так как у всех игроков одинаковый прогноз'
+        bot.telegram.sendMessage(process.env.GROUP_ID, groupMessage)
+        await truncateFreeStakes(sql)
+    } else {
+        let winnerStakes: IStake[] = []
+        for (let stake of stakes) {
+            if (winnerStakes.length == 0) winnerStakes.push(stake)
+            else if (parseFloat(stake.prediction) == parseFloat(winnerStakes[0].prediction)) winnerStakes.push(stake)
+            else break
+        }
+        await updateBalance(sql, -process.env.PRIZE_FOR_FREE_STAKE)
+        for (let stake of winnerStakes) {
+            let user = await getUser(sql, stake.userID)
+            if (user.referrer) {
+                let referrer = await getUser(sql, user.referrer)
+                let prizePerStake = +process.env.PRIZE_FOR_FREE_STAKE / winnerStakes.length
+                let userWinSum = Math.round(prizePerStake * (85/100))
+                let referrerPrize = Math.round(prizePerStake * (10/100))
+                let adminPrize = Math.round(prizePerStake * (5/100))
+                bot.telegram.sendMessage(user.id, '❇️ +' + userWinSum)
+                await updateUser(sql, user.id, 'balance', user.balance + userWinSum)
+                bot.telegram.sendMessage(referrer.id, '👥 +' + referrerPrize)
+                await updateUser(sql, user.referrer, 'balance', referrer.balance + referrerPrize)
+                await updateBalance(sql, +(adminPrize))
+            } else {
+                let prizePerStake = +process.env.PRIZE_FOR_FREE_STAKE / winnerStakes.length
+                let userWinSum = Math.round(prizePerStake * (85/100))
+                let adminPrize = Math.round(prizePerStake * (15/100))
+                bot.telegram.sendMessage(user.id, '❇️ +' + userWinSum)
+                await updateUser(sql, user.id, 'balance', user.balance + userWinSum)
+                await updateBalance(sql, +(adminPrize))
+            }
+            await updateBalance(sql, -(process.env.PRIZE_FOR_FREE_STAKE))
+            await updateUser(sql, user.id, 'wins', user.wins + 1)
+            let groupMessage =
+                '❇️ Пользователь ' + user.name + ' стал победителем в бесплатной игре!'
+            bot.telegram.sendMessage(process.env.GROUP_ID, groupMessage)
+        }
+        await truncateFreeStakes(sql)
+    }
+}
+
+async function playPaidGame() {
+    console.log('playPaidGame()')
+    let stakes = await getStakes(sql)
+    let players = []
+    let course: number
+    try {
+        course = await getCourse()
+        await bot.telegram.sendMessage(process.env.GROUP_ID, '💲 Курс: ' + parseFloat((course * 100).toFixed()) / 100)
+    } catch (e) {
+        let groupMessage =
+            '❌ Во время игры произошла ошибка. Все ставки возвращены игрокам'
+        bot.telegram.sendMessage(process.env.GROUP_ID, groupMessage)
+        let playersToClear = {}
+        for (let stake of stakes) {
+            if (!playersToClear[stake.userID.toString()]) {
+                playersToClear[stake.userID.toString()] = 1
+            } else {
+                playersToClear[stake.userID.toString()]++
+            }
+        }
+        for (let player in playersToClear) {
+            let user = await getUser(sql, +player)
+            await updateUser(sql, +player, 'balance', user.balance + 10000 * playersToClear[player])
+        }
+        await truncateStakes(sql)
+        return
+    }
+    let firstSortingIteration = true
+    stakes.sort((next, prev) => {
+        if (firstSortingIteration) {
+            firstSortingIteration = false
+            if (!players.includes(prev.userID)) players.push(prev.userID)
+            if (!players.includes(next.userID)) players.push(next.userID)
+        } else {
+            if (!players.includes(next.userID)) players.push(next.userID)
+        }
+        let prevDifferenceWithCourse = Math.abs(parseFloat(prev.prediction) - course)
+        let nextDifferenceWithCourse = Math.abs(parseFloat(next.prediction) - course)
+        return nextDifferenceWithCourse - prevDifferenceWithCourse
+    })
+
+    function isAllPredictionsEquals(stakes: IStake[]) {
+        let firstPrediction = stakes[0].prediction
+        for (let stake of stakes) {
+            if (stake.prediction != firstPrediction) return false
+        }
+        return true
+    }
+
+    if (players.length < 2) {
+        let groupMessage =
+            'ℹ️ Игра была отменена, так как не набралось нужное количество игроков'
+        bot.telegram.sendMessage(process.env.GROUP_ID, groupMessage)
+        let playersToClear = {}
+        for (let stake of stakes) {
+            if (!playersToClear[stake.userID.toString()]) {
+                playersToClear[stake.userID.toString()] = 1
+            } else {
+                playersToClear[stake.userID.toString()]++
+            }
+        }
+        for (let player in playersToClear) {
+            let user = await getUser(sql, +player)
+            await updateUser(sql, +player, 'balance', user.balance + 10000 * playersToClear[player])
+        }
+        await truncateStakes(sql)
+    } else if (isAllPredictionsEquals(stakes)) {
+        let groupMessage =
+            'ℹ️ Игра была отменена, так как не набралось нужное количество игроков'
+        bot.telegram.sendMessage(process.env.GROUP_ID, groupMessage)
+        let playersToClear = {}
+        for (let stake of stakes) {
+            if (!playersToClear[stake.userID.toString()]) {
+                playersToClear[stake.userID.toString()] = 1
+            } else {
+                playersToClear[stake.userID.toString()]++
+            }
+        }
+        for (let player in playersToClear) {
+            let user = await getUser(sql, +player)
+            await updateUser(sql, +player, 'balance', user.balance + 10000 * playersToClear[player])
+        }
+        await truncateStakes(sql)
+    } else {
+        let winnerStakes: IStake[] = []
+        let winnerPlayers: number[] = []
+        for (let stake of stakes) {
+            if (winnerStakes.length == 0) {
+                winnerStakes.push(stake)
+                winnerPlayers.push(stake.userID)
+            }
+            else if (parseFloat(stake.prediction) == parseFloat(winnerStakes[0].prediction)) {
+                winnerStakes.push(stake)
+                if (!winnerPlayers.includes(stake.userID)) winnerPlayers.push(stake.userID)
+            }
+            else break
+        }
+        console.log('Winners stakes: ', winnerStakes)
+        console.log('Winners players: ', winnerPlayers)
+        let playersAlreadyWon = []
+        let totalPrize = stakes.length * 10000
+        let prizePerStake = Math.round(totalPrize / winnerStakes.length)
+        for (let stake of winnerStakes) {
+            let user = await getUser(sql, stake.userID)
+            if (user.referrer) {
+                let referrer = await getUser(sql, user.referrer)
+                bot.telegram.sendMessage(user.id, '💸 +' + Math.round(prizePerStake * (85/100)))
+                await updateUser(sql, user.id, 'balance', user.balance + Math.round(prizePerStake * (85/100)))
+                bot.telegram.sendMessage(referrer.id, '👥 +' + Math.round(prizePerStake * (10/100)))
+                await updateUser(sql, referrer.id, 'balance', referrer.balance + Math.round(prizePerStake * (10/100)))
+                await updateBalance(sql, +Math.round(prizePerStake * (5/100)))
+            } else {
+                bot.telegram.sendMessage(user.id, '💸 +' + Math.round(prizePerStake * (85/100)))
+                await updateUser(sql, user.id, 'balance', user.balance + Math.round(prizePerStake * (85/100)))
+                await updateBalance(sql, +Math.round(prizePerStake * (15/100)))
+            }
+            if (!playersAlreadyWon.includes(stake.userID)) {
+                playersAlreadyWon.push(stake.userID)
+                let user = await getUser(sql, stake.userID)
+                await updateUser(sql, user.id, 'wins', user.wins + 1)
+                let groupMessage =
+                    '💸 Пользователь ' + user.name + ' стал победителем в игре!'
+                bot.telegram.sendMessage(process.env.GROUP_ID, groupMessage)
+            }
+        }
+        await truncateStakes(sql)
+    }
+
+}
+
+
+schedule.scheduleJob('0 0 15 * * *', stopGettingStakes)
+
+schedule.scheduleJob('0 0 20 * * *', playFreeGame)
+
+schedule.scheduleJob('0 0 21 * * *', playPaidGame)
+
+schedule.scheduleJob('0 3 21 * * *', startGettingStakes)
+
+// schedule.scheduleJob('0 40 10 * * *', stopGettingStakes)
+//
+// schedule.scheduleJob('0 41 10 * * *', playFreeGame)
+//
+// schedule.scheduleJob('0 42 10 * * *', playPaidGame)
+//
+// schedule.scheduleJob('0 43 10 * * *', startGettingStakes)
